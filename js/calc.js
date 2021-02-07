2ba919db3d490fbd1644a07f496790181c1a9aab
abrahamdev22 <45207776+abrahamdev22@users.noreply.github.com>
2021-02-07 17:30:57 +0700

    Masih step 1
    - tambah method tampilkan ke display input
    - tambah method hapus karakter

--- js/calc.js
+++ js/calc.js
@@ -1,4 +1,6 @@
 inputKey = document.querySelector('.calculator_keys');
+displayTop = '.input-display';
+
 
 let inputField = '';
 let inputSatu =''; //variable untuk menampung input numerik dan decimal dari user
@@ -14,6 +16,11 @@ inputKey.addEventListener('click', (e)=> {
     const keyContent = e.target.textContent;
     const keyDataSet = key.dataset.action;
     // console.log(keyDataSet);
+
+// Cek apakah tombol C diklik, bila ya, maka karakter terakhir yang diinputakan dihapus 
+if(keyDataSet==='delete'){
+    inputSatu = inputSatu.substring(0, inputSatu.length-1);   
+}
 
 
 // Step 1, Filter input , apakah numerik dan decimal
@@ -33,6 +40,11 @@ while(!keyDataSet){
 console.log('inputSatu:',inputSatu);
 
 
+// Tampilkan ke display
+tampilDisplayInput(inputSatu, displayTop);
+
+
+
 // Step 2, filter input, apakah tombol operator diklik
 // =============================================================================================================
 
@@ -48,4 +60,10 @@ function loadVariableInputSatu (inputUser){
     let input = '';
     input = inputUser;
     return input;
+}
+
+function tampilDisplayInput(input, displayCalc) {
+    let display = document.querySelector(displayCalc);
+    display.innerHTML = input;
+    // return display.innerHTML;
 }
